0a63cb558b402aaf99c3f0b5ee9df9b6a10f83f7
sffxzzp <sffxzzp@126.com>
2022-11-18 15:38:35 +0800

    upload

---
+++ EpicInfo.py
@@ -0,0 +1,30 @@
+import requests, math, json
+
+res = requests.post('https://graphql.epicgames.com/graphql', json={"query":"query catalogQuery($locale: String, $count: Int, $start: Int, $country: String!, $sortBy: String, $sortDir: String) {Catalog {searchStore(locale: $locale count: $count start: $start country: $country sortBy: $sortBy sortDir: $sortDir) {paging {count start total}}}}","variables":{"locale":"en","count":1,"start":0,"country":"US","sortBy":"lastModifiedDate","sortDir":"DESC"}}, verify=False, headers={'origin': 'https://epicgames.com'})
+res = res.json()
+total = res['data']['Catalog']['searchStore']['paging']['total']
+print('total: %d' % total)
+
+pages = math.ceil(total / 1000)
+offerid = {}
+namespace = {}
+for i in range(0, pages):
+	print('page: %d' % (i+1))
+	res = requests.post('https://graphql.epicgames.com/graphql', json={"query":"query catalogQuery($locale: String, $count: Int, $start: Int, $country: String!, $sortBy: String, $sortDir: String) {Catalog {searchStore(locale: $locale count: $count start: $start country: $country sortBy: $sortBy sortDir: $sortDir ) {elements {namespace urlSlug catalogNs {mappings {mappings {offerId} pageSlug}}} paging {count start total}}}}","variables":{"locale":"en","count":1000,"start":i*1000,"country":"US","sortBy":"lastModifiedDate","sortDir":"DESC"}}, verify=False, headers={'origin': 'https://epicgames.com'})
+	res = res.json()
+	for item in res['data']['Catalog']['searchStore']['elements']:
+		if item['catalogNs']['mappings'] != None:
+			for mapping in item['catalogNs']['mappings']:
+				if mapping['mappings']['offerId'] == None:
+					if mapping['pageSlug'] == item['urlSlug']:
+						if item['namespace'] not in namespace:
+							namespace[item['namespace']] = mapping['pageSlug']
+				else:
+					if mapping['mappings']['offerId'] not in offerid:
+						offerid[mapping['mappings']['offerId']] = mapping['pageSlug']
+
+with open('offerid.json', 'w', encoding='utf-8') as f:
+	f.write(json.dumps(offerid))
+
+with open('namespace.json', 'w', encoding='utf-8') as f:
+	f.write(json.dumps(namespace))
---
+++ README.md
@@ -0,0 +1,9 @@
+EpicInfo
+======
+将匹配于[其乐社区](https://keylol.com)中的 Epic 游戏链接，并变更已拥有游戏的链接颜色。
+
+同步网页使用[Steam 帐号信息同步](https://steamdb.keylol.com/sync)页面。
+
+脚本发布页面：[\[脚本\]Epic拥有游戏链接变色](https://keylol.com/t640860-1-1)
+
+自动构建状态：todo
